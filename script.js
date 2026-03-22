7836e10077343bc935088b27a1a0784d4829ad7e
aegon78 <204925927+aegon78@users.noreply.github.com>
2026-03-22 08:58:37 +0100

    ui fixes

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const navToggle = document.querySelector('.nav-toggle')
+const sidebar = document.querySelector('.links-container')
+
+navToggle.addEventListener('click', ()=>{
+    sidebar.classList.toggle('active')
+})
+
 const levelHeaders = document.querySelectorAll('.level-header')
 const levelBodies = document.querySelectorAll('.level-body')
 
