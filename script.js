0f6a3df79cc6d8188975de3b00691a96a7265e06
aegon78 <204925927+aegon78@users.noreply.github.com>
2026-03-25 17:11:58 +0100

    added data.json (SoT)

--- script.js
+++ script.js
@@ -13,6 +13,50 @@ overlay.addEventListener('click', ()=>{
 })
 
 
+async function renderUnits(){
+    const response = await fetch('./data.json')
+    const data = await response.json()
+    
+    const levelsContainer = document.querySelector('.levels')
+    const levelsArr = data.levels
+    
+    levelsArr.forEach((level, idx) =>{
+
+        const levelDiv = document.createElement('div')
+        levelDiv.classList.add('level', `level-${idx + 1}`)
+        levelsContainer.appendChild(levelDiv)
+        
+        const levelHeaderDiv = document.createElement('div')
+        const levelBodyDiv = document.createElement('div')
+        
+        levelHeaderDiv.classList.add('level-header', 'show')
+        levelBodyDiv.classList.add('level-body')
+
+        levelDiv.append(levelHeaderDiv, levelBodyDiv)
+
+        const titleContainerH2 = document.createElement('h2')
+        titleContainerH2.innerHTML = `
+            <span class="ficon level-icon">${level.levelicon}</span>
+            ${level.title}
+        `
+        const levelToggle = document.createElement('button')
+        levelToggle.classList.add('level-toggle')
+        levelHeaderDiv.append(titleContainerH2, levelToggle)
+    
+        level.units.forEach(unit =>{
+            unitContainer = document.createElement('a')
+            unitContainer.classList.add('unit')
+            unitContainer.innerHTML = `
+                <span class="svg-container">
+                    ${unit.icon}
+                </span>
+                <p class="unit-title">${unit.title}</p>
+            `
+            levelBodyDiv.appendChild(unitContainer)
+        })
+    })
+}
+renderUnits()
 
 const levelHeaders = document.querySelectorAll('.level-header')
 const levelBodies = document.querySelectorAll('.level-body')
@@ -20,6 +64,7 @@ const levelBodies = document.querySelectorAll('.level-body')
 let collapseStatus = JSON.parse(localStorage.getItem('collapse_status')) || {}
 
 levelHeaders.forEach((header, idx) =>{
+    //this line checks when the user first enter the page
     
     if(collapseStatus[idx] === true){
         header.classList.add('show')
