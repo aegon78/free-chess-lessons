42cd74666ecb08dc34bb35a4284e5d37a5010782
aegon78 <204925927+aegon78@users.noreply.github.com>
2026-03-29 17:08:49 +0100

    added query string!

--- script.js
+++ script.js
@@ -1,107 +1,109 @@
-// const navbar = document.querySelector('.navbar')
-// const navToggle = document.querySelectorAll('.nav-toggle')
-// const sidebar = document.querySelector('.links-container')
-// const overlay = document.querySelector('.overlay')
+const navbar = document.querySelector('.navbar')
+const navToggle = document.querySelectorAll('.nav-toggle')
+const sidebar = document.querySelector('.links-container')
+const overlay = document.querySelector('.overlay')
 
-// let lastScrollY = window.scrollY
+let lastScrollY = window.scrollY
 
-// window.addEventListener('scroll', ()=>{
+window.addEventListener('scroll', ()=>{
     
-//     if(lastScrollY < window.scrollY){
-//         navbar.classList.add('hide')
-//     }else if(lastScrollY > window.scrollY){
-//         navbar.classList.remove('hide')
-//     }
+    if(lastScrollY < window.scrollY){
+        navbar.classList.add('hide')
+    }else if(lastScrollY > window.scrollY){
+        navbar.classList.remove('hide')
+    }
 
-//     lastScrollY = window.scrollY
-// })
-
-// navToggle.forEach(nt =>{
-//     nt.addEventListener('click', ()=>{
-//         sidebar.classList.toggle('active')
-//     })
-// })
-
-// overlay.addEventListener('click', ()=>{
-//     sidebar.classList.remove('active')
-// })
-
-
-// async function renderUnits(){
-//     const response = await fetch('./data.json')
-//     const data = await response.json()
-    
-//     const levelsContainer = document.querySelector('.levels')
-//     const levelsArr = data.levels
-    
-//     levelsArr.forEach((level, idx) =>{
-
-//         const levelDiv = document.createElement('div')
-//         levelDiv.classList.add('level', `level-${idx + 1}`)
-//         levelsContainer.appendChild(levelDiv)
-        
-//         const levelHeaderDiv = document.createElement('div')
-//         const levelBodyDiv = document.createElement('div')
-        
-//         levelHeaderDiv.classList.add('level-header')
-//         levelBodyDiv.classList.add('level-body')
-
-//         levelDiv.append(levelHeaderDiv, levelBodyDiv)
-
-//         const titleContainerH2 = document.createElement('h2')
-//         titleContainerH2.innerHTML = `
-//             <span class="ficon level-icon">${level.levelicon}</span>
-//             ${level.title}
-//         `
-//         const levelToggle = document.createElement('button')
-//         levelToggle.classList.add('level-toggle')
-//         levelHeaderDiv.append(titleContainerH2, levelToggle)
-    
-//         level.units.forEach(unit =>{
-//             const unitContainer = document.createElement('a')
-//             unitContainer.classList.add('unit')
-//             unitContainer.innerHTML = `
-//                 <span class="svg-container">   
-                    
-//                     <svg class="unit-icon">
-//                         <use href="icons.svg#${unit.icon}"></use>
-//                     </svg>
-//                 </span>
-//                 <p class="unit-title">${unit.title}</p>
-//             `
-//             levelBodyDiv.appendChild(unitContainer)
-//         })    
-//     })
-//     const levelHeaders = document.querySelectorAll('.level-header')
-//     const levelBodies = document.querySelectorAll('.level-body')
-
-//     let collapseStatus = JSON.parse(localStorage.getItem('collapse_status')) || {}
-
-//     levelHeaders.forEach((header, idx) =>{
-//         if(collapseStatus[idx] !== false){//this line checks when the user first enter the page
-//             header.classList.add('show')
-//         }
-        
-//         header.addEventListener('click', ()=>{
-            
-//             const isCollapsed = header.classList.toggle('show')
-
-//             collapseStatus[idx] = isCollapsed
-
-//             localStorage.setItem('collapse_status', JSON.stringify(collapseStatus))
-//         })
-// })
-// }
-// renderUnits()
-
-
-const btcAddress = document.querySelector('.btc-address').innerText
-const copyBtn = document.querySelector('.svgs-container')
-
-copyBtn.addEventListener('click', ()=>{
-    navigator.clipboard.writeText(btcAddress)
-    
-    copyBtn.classList.add('copied')
-
-    setTimeout(()=> copyBtn.classList.remove('copied'), 3000);
+    lastScrollY = window.scrollY
 })
+
+navToggle.forEach(nt =>{
+    nt.addEventListener('click', ()=>{
+        sidebar.classList.toggle('active')
+    })
+})
+
+overlay.addEventListener('click', ()=>{
+    sidebar.classList.remove('active')
+})
+
+function renderPage(){
+    const bodyEl = document.querySelector('body')
+    const params = new URLSearchParams(window.location.search)
+    const page = params.get('p') || 'home'
+
+    if(page === 'donate'){
+        bodyEl.innerHTML = "test!"
+    }else{
+        renderUnits()
+    }
+    
+}
+
+renderPage()
+
+async function renderUnits(){
+    const response = await fetch('./data.json')
+    const data = await response.json()
+    
+    const levelsContainer = document.querySelector('.levels')
+    const levelsArr = data.levels
+    
+    levelsArr.forEach((level, idx) =>{
+
+        const levelDiv = document.createElement('div')
+        levelDiv.classList.add('level', `level-${idx + 1}`)
+        levelsContainer.appendChild(levelDiv)
+        
+        const levelHeaderDiv = document.createElement('div')
+        const levelBodyDiv = document.createElement('div')
+        
+        levelHeaderDiv.classList.add('level-header')
+        levelBodyDiv.classList.add('level-body')
+
+        levelDiv.append(levelHeaderDiv, levelBodyDiv)
+
+        const titleContainerH2 = document.createElement('h2')
+        titleContainerH2.innerHTML = `
+            <span class="ficon level-icon">${level.levelicon}</span>
+            ${level.title}
+        `
+        const levelToggle = document.createElement('button')
+        levelToggle.classList.add('level-toggle')
+        levelHeaderDiv.append(titleContainerH2, levelToggle)
+    
+        level.units.forEach(unit =>{
+            const unitContainer = document.createElement('a')
+            unitContainer.classList.add('unit')
+            unitContainer.innerHTML = `
+                <span class="svg-container">   
+                    
+                    <svg class="unit-icon">
+                        <use href="icons.svg#${unit.icon}"></use>
+                    </svg>
+                </span>
+                <p class="unit-title">${unit.title}</p>
+            `
+            levelBodyDiv.appendChild(unitContainer)
+        })    
+    })
+    const levelHeaders = document.querySelectorAll('.level-header')
+    const levelBodies = document.querySelectorAll('.level-body')
+
+    let collapseStatus = JSON.parse(localStorage.getItem('collapse_status')) || {}
+
+    levelHeaders.forEach((header, idx) =>{
+        if(collapseStatus[idx] !== false){//this line checks when the user first enter the page
+            header.classList.add('show')
+        }
+        
+        header.addEventListener('click', ()=>{
+            
+            const isCollapsed = header.classList.toggle('show')
+
+            collapseStatus[idx] = isCollapsed
+
+            localStorage.setItem('collapse_status', JSON.stringify(collapseStatus))
+        })
+})
+}
+renderUnits()
