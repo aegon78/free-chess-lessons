7f4c609df9db266f822dc87cebbff26a87cf5d58
aegon78 <204925927+aegon78@users.noreply.github.com>
2026-04-01 09:07:17 +0100

    course page rendering

---
+++ course.js
@@ -0,0 +1,71 @@
+const params = new URLSearchParams(window.location.search)
+const unitSlug = params.get('u')
+
+async function initUnit(){
+    const response = await fetch('data.json')
+    const data = await response.json()
+
+    data.levels.forEach(level =>{
+        level.units.forEach(unit =>{
+            currentTitle = unit.title.toLowerCase().replace(/\s+/g, '-')
+
+            if(currentTitle === unitSlug){
+                renderUnit(level, unit)
+            }
+        })
+    })
+
+}
+initUnit()
+
+function renderUnit(level, unit){
+    const bodyEl = document.querySelector('body')
+    const unitCard = document.querySelector('.unit-card')
+    unitCard.innerHTML = ''
+
+    //creating the level name at top and appending it
+    const levelName = document.createElement('h2')
+    levelName.classList.add('level-name')
+    levelName.innerHTML = `
+        <span class="level-icon ficon">${level.levelicon}</span>${level.title}
+    `
+    unitCard.append(levelName)
+
+    //creating the circle and the icon and appending them
+    const unitIconContainer = document.createElement('div')
+    unitIconContainer.classList.add('unit-icon-container')
+    unitIconContainer.innerHTML = `
+        <span>   
+            <svg class="theSVG">
+                <use href="assets/icons.svg#${unit.icon}"></use>
+            </svg>
+        </span>
+    `
+    unitCard.append(unitIconContainer)
+
+    //creating the title and appending it.
+    const unitTitle = document.createElement('h2')
+    unitTitle.classList.add('unit-title')
+    unitTitle.innerText = unit.title
+    unitCard.append(unitTitle)
+
+    //creating lessons container and the lessons inside and appending them.
+    const lessonsContainer = document.createElement('div')
+    lessonsContainer.classList.add('lessons-container')
+    bodyEl.append(lessonsContainer)
+    unit.lessons.forEach(lesson =>{
+        const lessonCard = document.createElement('div')
+        lessonCard.classList.add('lesson-card')
+        lessonCard.innerHTML = `
+            <div class="lesson-thumbnail"><span class="play-icon ficon">J</span></div>
+        `
+        const lessonDetailsContainer = document.createElement('div')
+        lessonDetailsContainer.innerHTML = `
+                <h3 class="lesson-title">${lesson.title}</h3>
+                <p class="lesson-description">${lesson.description}</p>
+
+        `
+        lessonCard.append(lessonDetailsContainer)
+        lessonsContainer.append(lessonCard)
+    })
+}
